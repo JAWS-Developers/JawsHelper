944a3b0733d66d9319fd58966d3f8e6900f4475b
Timo Coupek <t.coupek@gmail.com>
2024-08-10 21:53:13 +0200

    Version: 1.0.1-8 -

--- scripts/add-script.js
+++ scripts/add-script.js
@@ -6,11 +6,10 @@ const packageJsonPath = path.resolve(process.cwd(), 'package.json');
 
 // Leggi il package.json del progetto parent
 let packageJson = require(packageJsonPath);
-const scriptPath = path.resolve(__dirname, './main.js');
 
 // Aggiungi lo script per avviare il progetto
 packageJson.scripts = packageJson.scripts || {};
-packageJson.scripts["start-project"] = `node ${scriptPath}`;
+packageJson.scripts["release"] = `node ./node_modules/jaws-release-manager/main.js`;
 
 // Scrivi il package.json aggiornato
 fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
